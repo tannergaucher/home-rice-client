c91c471746711f9d5ba70e7bef0d2eba4ffbb44e
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-10-11 22:33:09 -0400

    Update hostname in yt embed

--- src/templates/recipe-template.js
+++ src/templates/recipe-template.js
@@ -18,7 +18,7 @@ export default function RecipeTemplate({ data }) {
             className="responsive-iframe"
             id="player"
             type="text/html"
-            src={`http://www.youtube.com/embed/${data.sanityRecipe.youtubeVideoId}?enablejsapi=1&origin=http://localhost:8000&modestbranding=1`}
+            src={`http://www.youtube.com/embed/${data.sanityRecipe.youtubeVideoId}?enablejsapi=1&origin=https://tg-platform.netlify.app&modestbranding=1`}
             frameBorder="0"
           ></iframe>
         </div>
