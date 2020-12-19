43aa56bb438288e75b2f8be6e4424cdf536b3618
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-12-19 01:34:54 -0500

    Update  design

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import "semantic-styles"
 import "./index.css"
@@ -7,13 +6,6 @@ import "./index.css"
 const Layout = ({ children }) => {
   return (
     <div>
-      <header className="header padding container">
-        {/* <Link to="/" style={{ textDecoration: `none` }}>
-          <h1 className="text--xxxl title" style={{ fontStyle: `italic` }}>
-            Home Rice
-          </h1>
-        </Link> */}
-      </header>
       <main className="main">{children}</main>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,49 @@ import React from "react"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
 
-import { Brand, Layout } from "../components"
+import { Layout, Brand } from "../components"
 
 export default function IndexPage({ data }) {
   return (
     <Layout>
-      <article className="page">
-        <section>
-          <Brand />
-        </section>
-        <br />
-        <div
-          className="container content-grid only-mobile-padding"
-          style={{ marginTop: `var(--space-lg)` }}
-        >
+      <article>
+        <Brand />
+        <div style={{ padding: `var(--padding)` }}>
           {data.allSanityVideo.edges.map(edge => (
-            <div className="card">
-              <Link
-                key={edge.node.id}
-                style={{ textDecoration: `none` }}
-                to={`/${edge.node.slug.current}`}
-              >
+            <Link
+              key={edge.node.id}
+              style={{ textDecoration: `none` }}
+              to={`/${edge.node.slug.current}`}
+            >
+              <div style={{ position: `relative` }}>
+                <h2
+                  className="responsive-title"
+                  style={{
+                    position: `absolute`,
+                    top: `50%`,
+                    left: `50%`,
+                    transform: `translate(-50%, -50%)`,
+                    zIndex: 3,
+                    textAlign: `center`,
+                    lineHeight: 1,
+                    marginBlockStart: 0,
+                    marginBlockEnd: 0,
+                  }}
+                >
+                  {edge.node.title}
+                </h2>
                 <Img
+                  className="index-img"
                   fluid={edge.node.mainImage.asset.fluid}
-                  style={{ borderRadius: `var(--radius)` }}
+                  style={{
+                    borderRadius: `var(--radius)`,
+                    filter: `brightness(0.5)`,
+                  }}
                 />
-              </Link>
-            </div>
+              </div>
+            </Link>
           ))}
         </div>
-        <br />
       </article>
     </Layout>
   )
