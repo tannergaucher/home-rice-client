3110ba181f55ec61f8688a4fabc4c881ed98621d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2021-03-22 17:13:31 -0400

    Render Amazon CTA btn when amazon ingredients

--- src/components/ingredients-form.js
+++ src/components/ingredients-form.js
@@ -2,26 +2,34 @@ import React from "react"
 
 import { AWS_ASSOCIATE_ID } from "../utils/constants"
 
-export default function IngredientsForm({ children }) {
+export default function IngredientsForm({ ingredients, children }) {
+  const amazonIngredients = ingredients.filter(
+    ingredient => ingredient.ASIN !== null
+  )
+
   return (
     <form
       method="GET"
       action="https://www.amazon.com/gp/aws/cart/add.html"
       style={{ position: `relative` }}
     >
-      <button
-        style={{
-          width: `100%`,
-          marginBottom: `0`,
-        }}
-        className="btn btn-primary"
-        type="submit"
-        name="add"
-      >
-        Ingredients on Amazon
-      </button>
-      <input type="hidden" name="AssociateTag" value={AWS_ASSOCIATE_ID} />
-      <hr className="hr" />
+      {amazonIngredients.length > 0 && (
+        <>
+          <button
+            style={{
+              width: `100%`,
+              marginBottom: `0`,
+            }}
+            className="btn btn-primary"
+            type="submit"
+            name="add"
+          >
+            Ingredients on Amazon
+          </button>
+          <input type="hidden" name="AssociateTag" value={AWS_ASSOCIATE_ID} />
+          <hr className="hr" />
+        </>
+      )}
       {children}
     </form>
   )
