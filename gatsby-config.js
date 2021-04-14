90cfa80c124cd311d248f98493574936fffc93c1
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2021-04-14 08:59:33 -0400

    Remove unused dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
-
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
